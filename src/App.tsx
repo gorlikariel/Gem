a1b90940e04cb8b73ced05931735e36ebb4b6702
Ariel <gorlikariel@gmail.com>
2018-08-09 19:37:23 +0300

    poop v1.1

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+class App extends React.Component {
+  public render() {
+    return (
+      <div>
+        <h1>SHIT FUCK</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
