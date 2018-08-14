befa88cd08440fc8a9128e08e8254389965715e8
Ariel <gorlikariel@gmail.com>
2018-08-14 13:21:23 +0300

    initial folder setup

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,32 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
+// import { BrowserRouter } from "react-router-dom";
+// import { Provider } from "react-redux";
+// import { createStore, combineReducers, applyMiddleware, compose } from "redux";
+// import BurgerBuilderReducer from "./store/reducers/BurgerBuilderReducer";
+// import ContactDataReducer from "./store/reducers/ContactDataReducer";
+// import thunk from "redux-thunk";
+// import AuthReducer from "./store/reducers/AuthReducer";
 
-ReactDOM.render(<App />, document.getElementById("root") as HTMLElement);
-registerServiceWorker();
+// const rootReducer = combineReducers({
+//   builder: BurgerBuilderReducer,
+//   form: ContactDataReducer,
+//   auth: AuthReducer
+// });
+
+// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+// const store = createStore(
+//   rootReducer,
+//   composeEnhancers(applyMiddleware(thunk))
+// );
+const app = (
+  // <Provider store={store}>
+  // <BrowserRouter>
+  <App />
+  // </BrowserRouter>
+  // </Provider>
+);
+
+ReactDOM.render(app, document.getElementById("root"));
